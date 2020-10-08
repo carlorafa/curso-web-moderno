a2dd5d3f190a1eae316ae702f9523b320efdfb5c
Rafael Miranda <carlorafa@gmail.com>
2020-10-08 14:12:00 -0300

    funcao.js

---
+++ funcao.js
@@ -0,0 +1,20 @@
+//funcao de forma literal 
+function fun1(){}
+
+//armazenar em uma variãvel 
+//para executar fun2()
+//para passar por parametro fun2
+const fun2 = function(){}
+
+//armazenar em um atributo de obj
+
+const obj = {}
+obj.falar = function(){return 'Opa'}
+console.log(obj.falar())
+
+
+
+
+
+
+
